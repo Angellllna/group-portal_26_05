f59bfc2c8adbc804ee42fe7fa983126e837e2f94
Maksss <hvcug4462@gmail.com>
2026-05-28 19:35:11 +0300

    GAL-2 added front + views + app urls

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("gallery/", include("gallery.urls", namespace="gallery")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = [...] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import MediaItem
 
-# Create your views here.
+
+class MediaListView(ListView):
+    model = MediaItem
+    template_name = "gallery/list.html"
+    context_object_name = "media_items"
+    paginate_by = 12
+
+    def get_queryset(self):
+        return (
+            MediaItem.objects.filter(is_approved=True)
+            .select_related("author")
+            .order_by("-created_at")
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["media_type_filter"] = self.request.GET.get("type", "")
+        context["media_types"] = MediaItem.MediaType.choices
+        return context
+
+
+class MediaDetailView(DetailView):
+    model = MediaItem
+    template_name = "gallery/detail.html"
+    context_object_name = "item"
+    pk_url_kwarg = "id"
+
+    def get_queryset(self):
+        return MediaItem.objects.filter(is_approved=True).select_related("author")
