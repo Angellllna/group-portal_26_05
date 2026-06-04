eb4ca90db30a97c10dd838edfaebce5e5f18145e
Kardinm <vovamakodau@gmail.com>
2026-06-04 19:31:30 +0300

    Add portfolio update functionality with form and template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, UpdateView
+
 from .models import Portfolio
+from .forms import PortfolioForm
 
 
 class PortfolioListView(ListView):
@@ -14,3 +18,26 @@ class PortfolioDetailView(DetailView):
     template_name = "portfolio/detail.html"
     context_object_name = "portfolio"
     pk_url_kwarg = "id"
+
+
+class PortfolioUpdateView(LoginRequiredMixin, UpdateView):
+    model = Portfolio
+    form_class = PortfolioForm
+    template_name = "portfolio/edit.html"
+
+
+    def get_object(self, queryset=None):
+        portfolio, _ = Portfolio.objects.get_or_create(
+            user=self.request.user,
+            defaults={
+                "codename": self.request.user.username,
+                "specialization": "gunsmith",
+                "level": 1,
+            }
+        )
+
+        return portfolio
+
+
+    def get_success_url(self):
+        return reverse("portfolio:detail", kwargs={"id": self.object.id})
