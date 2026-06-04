1d3bffb4ae5524ac0a12415840821c925cfc68f8
Anhelina Sadvari <sadvari.anhelina@student.uzhnu.edu.ua>
2026-06-04 15:27:31 +0200

    Wire up portfolio URLs and unify quote style
    - Include portfolio.urls in config/urls.py under the "portfolio/" prefix
      with the "portfolio" namespace. Without this the view pages were
      unreachable (404) and any {% url 'portfolio:... %} raised NoReverseMatch.
    - portfolio/urls.py: switch single quotes to double quotes per project
      convention.

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("", include("core.urls")),
     path("admin/", admin.site.urls),
     path("gallery/", include("gallery.urls", namespace="gallery")),
+    path("portfolio/", include("portfolio.urls", namespace="portfolio")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import PortfolioListView, PortfolioDetailView
 
-app_name = 'portfolio'
+app_name = "portfolio"
 
 urlpatterns = [
-    path('', PortfolioListView.as_view(), name='list'),
-    path('<int:id>/', PortfolioDetailView.as_view(), name='detail'),
+    path("", PortfolioListView.as_view(), name="list"),
+    path("<int:id>/", PortfolioDetailView.as_view(), name="detail"),
 ]
