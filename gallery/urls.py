e5d89f72cf7b02373b47d0948c0c084635f79b5a
Maksss <hvcug4462@gmail.com>
2026-06-04 19:43:31 +0300

    GAL-3 added forms upload js and html, updated views and urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MediaListView, MediaDetailView
+from .views import MediaListView, MediaDetailView, MediaUploadView
 
 app_name = "gallery"
 
 urlpatterns = [
     path("", MediaListView.as_view(), name="list"),
+    path("upload/", MediaUploadView.as_view(), name="upload"),
     path("<int:id>/", MediaDetailView.as_view(), name="detail"),
 ]
